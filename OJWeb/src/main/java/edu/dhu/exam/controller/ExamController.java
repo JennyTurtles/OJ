90fe120e19ae7aa3705e62dcc536018f48fc8a53
gongrunze <823650460@qq.com>
2023-10-24 14:27:14 +0800

    /exam/examList

--- OJWeb/src/main/java/edu/dhu/exam/controller/ExamController.java
+++ OJWeb/src/main/java/edu/dhu/exam/controller/ExamController.java
@@ -6,6 +6,7 @@ import edu.dhu.exam.service.ExamService;
 import edu.dhu.global.util.DecodeToken;
 import edu.dhu.user.model.RespBean;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -25,8 +26,12 @@ public class ExamController {
             return RespBean.error("考试ID不存在！");
     }
 
-    @PostMapping("/examList")
-    public RespBean getExamList(@RequestBody PMExam pMExam, HttpServletRequest request) {
+    @GetMapping("/examList")
+    @Transactional
+    public RespBean getExamList(Integer pageNum,Integer pageSize, HttpServletRequest request) {
+        PMExam pMExam = new PMExam();
+        pMExam.setPage(pageNum);
+        pMExam.setRows(pageSize);
         DecodeToken decodeToken = new DecodeToken(request);
         String userId = decodeToken.getUserId();
         pMExam.setStudentId(Integer.parseInt(userId));
